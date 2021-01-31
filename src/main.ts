62e75ace0d104f00448ec6d46ae3631fe961e080
BO <bwang0629@gmail.com>
2021-01-31 16:41:04 +0800

    require()

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,17 @@ import router from "./router";
 import store from "./store";
 import { SET_MENULIST, SET_ROLE } from "@/store/VuexTypes";
 
-function init() {
-  createApp(App)
-    .use(store)
-    .use(router)
-    .mount("#app");
+const role = sessionStorage.getItem("role");
+
+if (role) {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const user = require("@/role/UserRole.ts");
+  const menuList = user.setUserRole();
+  store.commit(SET_ROLE, role);
+  store.commit(SET_MENULIST, menuList);
 }
 
-const role = sessionStorage.getItem("role");
-if (role) {
-  import("@/role/UserRole.ts").then(({ setUserRole }) => {
-    const menuList = setUserRole();
-    store.commit(SET_ROLE, role);
-    store.commit(SET_MENULIST, menuList);
-    init();
-  });
-} else {
-  init();
-}
+createApp(App)
+  .use(store)
+  .use(router)
+  .mount("#app");
